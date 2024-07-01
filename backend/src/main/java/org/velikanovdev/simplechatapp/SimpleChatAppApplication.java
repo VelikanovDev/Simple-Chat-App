8fdfb73e9218606d809e3289aaa07e1bc9a5714f
Aleksei Velikanov <velikanov.dev@gmail.com>
2024-07-01 19:28:51 +0200

    add websocket configs

---
+++ backend/src/main/java/org/velikanovdev/simplechatapp/SimpleChatAppApplication.java
@@ -0,0 +1,11 @@
+package org.velikanovdev.simplechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleChatAppApplication.class, args);
+    }
+}
